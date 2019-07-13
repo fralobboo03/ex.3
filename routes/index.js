bf912a60941ddd1fb763f8d1e665c143f4829b59
Kittisak Kittisak <59143205@g.amru.ac.th>
2019-07-13 16:11:34 +0700

    เพิ่มdatabase

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var contact = require ('../config/database');
+
+router.get('/', function(req, res, next) {
+  var query = "SELECT * FROM positions";
+  contact.query(query, function(error,result){
+    res.json(result)
+  
+  })
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,5 +22,8 @@ router.get('/about', function(req, res, next) {
 router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'ติดต่อยังไงครับคุณแจ็คครับ' });
 });
+
+
+
 
 module.exports = router;
